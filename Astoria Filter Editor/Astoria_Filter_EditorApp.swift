fc0f749ea14552f0c745cf31c3476ecbecaff482
James Majors <noizetoys@mac.om>
2025-11-08 16:32:57 -0500

    Initial Commit

---
+++ Astoria Filter Editor/Astoria_Filter_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Astoria_Filter_EditorApp.swift
+//  Astoria Filter Editor
+//
+//  Created by James Majors on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Astoria_Filter_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Astoria Filter EditorTests/Astoria_Filter_EditorTests.swift
@@ -0,0 +1,16 @@
+//
+//  Astoria_Filter_EditorTests.swift
+//  Astoria Filter EditorTests
+//
+//  Created by James Majors on 11/8/25.
+//
+
+import Testing
+
+struct Astoria_Filter_EditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
