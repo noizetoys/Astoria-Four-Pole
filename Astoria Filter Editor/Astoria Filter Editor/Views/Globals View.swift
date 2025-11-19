4d5aaa1f34bd47e3263153cbda5875647265610a
James Majors <noizetoys@yahoo.com>
2025-11-19 12:24:49 -0500

    Adds Globals (No transmission)

---
+++ Astoria Filter Editor/Astoria Filter Editor/Views/Globals View.swift
@@ -0,0 +1,18 @@
+//
+//  Globals View.swift
+//  Astoria Filter Editor
+//
+//  Created by James B. Majors on 11/19/25.
+//
+
+import SwiftUI
+
+struct Globals_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Globals_View()
+}
