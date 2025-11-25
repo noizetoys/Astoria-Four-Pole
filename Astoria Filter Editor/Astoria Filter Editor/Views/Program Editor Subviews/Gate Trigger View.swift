3717a50b6cdb827f1af71c69b1cc513e589546e5
James Majors <noizetoys@yahoo.com>
2025-11-24 22:57:54 -0500

    Updates and corrects Gate Time, Trigger Source, Trigger mode to correct bindings

---
+++ Astoria Filter Editor/Astoria Filter Editor/Views/Program Editor Subviews/Gate Trigger View.swift
@@ -0,0 +1,18 @@
+//
+//  Gate Trigger View.swift
+//  Astoria Filter Editor
+//
+//  Created by James B. Majors on 11/24/25.
+//
+
+import SwiftUI
+
+struct Gate_Trigger_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Gate_Trigger_View()
+}
